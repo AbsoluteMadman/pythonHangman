6857b3e7603de2db80b4c8b970eed70ae955cabd
AbsoluteMadman <25347422+AbsoluteMadman@users.noreply.github.com>
2021-09-24 20:40:19 +0100

    Added update_blanks and update_board functions

--- python_hangman.py
+++ python_hangman.py
@@ -28,20 +28,20 @@ def game_title():  #Prints beautiful ascii art
 
 def get_word(category):  #Returns a random word from a specific list
     word_list = ['creature', 'faithful', 'crown', 'engine', 'divergent', 'gabby', 'cough', 'natural', 'tenuous', 'expensive', 'bucket', 'square', 'historical', 'unequaled', 'pizzas', 'trite', 'reaction', 'uppity', 'overwrought', 'waiting', 'rifle']
-    animals = ['cat', 'dog', 'giraffe', 'aardvark', 'lion', 'rat', 'frog', 'pig', 'cow', 'horse', 'whale', 'spider', 'squirrel', 'octopus', 'jellyfish', 'zebra']
-    cars = ['ford', 'ferarri', 'nissan', 'toyota', 'opel', 'skoda', 'volkswagen', 'mercedes', 'fiat', 'saab', 'lincoln', 'lexus', 'porsche', 'audi', 'bmw', 'renault', 'honda']
-    countries = ['ireland', 'canada', 'ghana', 'france', 'russia', 'china', 'tibet', 'taiwan', 'japan', 'germany', 'spain', 'mexico', 'zimbabwe', 'israel', 'croatia']
+    animals = ['cat', 'dog', 'giraffe', 'aardvark', 'lion', 'rat', 'frog', 'pig', 'cow', 'horse', 'whale', 'spider', 'squirrel', 'octopus', 'jellyfish', 'zebra', 'chimpanzee', 'raccoon', 'ocelot', 'platypus']
+    cars = ['ford', 'ferarri', 'nissan', 'toyota', 'opel', 'skoda', 'volkswagen', 'mercedes', 'fiat', 'saab', 'lincoln', 'lexus', 'porsche', 'audi', 'bmw', 'renault', 'honda', 'lamborghini', 'koenigsegg', 'bugatti']
+    countries = ['ireland', 'canada', 'ghana', 'france', 'russia', 'china', 'tibet', 'taiwan', 'japan', 'germany', 'spain', 'mexico', 'zimbabwe', 'israel', 'croatia', 'hungary', 'australia', 'sealand', 'greenland', 'sweden']
 
     if category == 1:
-        word_num = random.randrange(0, len(animals))
+        word_num = random.randrange(0, len(animals) - 1)
         random_word = animals[word_num]
 
     elif category == 2:
-        word_num = random.randrange(0, len(cars))
+        word_num = random.randrange(0, len(cars) - 1)
         random_word = cars[word_num]
 
     elif category == 3:
-        word_num = random.randrange(0, len(countries))
+        word_num = random.randrange(0, len(countries) - 1)
         random_word = countries[word_num]
 
     else:
@@ -76,30 +76,185 @@ def get_blanks(game_word):
     
     return game_blanks
 
-def update_blanks(new_letter, game_blanks, game_word):
-    updated_blanks = ""
-    #append game_blanks with new letter in x position
-    return updated_blanks
+def update_blanks(new_letter, game_word): #finds the guessed letter in the game word and creates new blanks with the new letter
+    game_blanks = []
+    game_blanks_string = ""
+    for letter in game_word:
+        game_blanks.append('_ ')
+    
+    for i in range(0, len(game_word) - 1):
+        if game_word[i] == new_letter:
+            game_blanks[i] = new_letter + ' '
+
+    for letter in game_blanks:
+        game_blanks_string += letter
+
+    return game_blanks_string
+
+def update_board(wrong_count):   #Updates the hangman board state
+    hangman = ""
+
+    if wrong_count == 0:
+        hangman = """
+        
+            
+            
+            
+             
+             
+        ____|____     
+"""
+    elif wrong_count == 1:
+        hangman = """
+        
+            
+        
+            
+            |
+            |
+        ____|____     
+"""
+    elif wrong_count == 2:
+        hangman = """
+        
+            
+            |
+            |
+            |  
+            |  
+        ____|____     
+"""
+
+    elif wrong_count == 3:
+        hangman = """
+        
+            _____
+            |
+            |   
+            |  
+            |  
+        ____|____     
+"""
+
+    elif wrong_count == 4:
+        hangman = """
+        
+            _____
+            |/  
+            |   
+            |  
+            |  
+        ____|____     
+"""
+
+    elif wrong_count == 5:
+        hangman = """
+        
+            _____
+            |/  
+            |   o
+            | 
+            |  
+        ____|____     
+"""
+
+    elif wrong_count == 6:
+        hangman = """
+        
+            _____
+            |/  
+            |   o
+            |   |
+            |   
+        ____|____     
+"""
+
+    elif wrong_count == 7:
+        hangman = """
+        
+            _____
+            |/  
+            |   o
+            |  /| 
+            |   
+        ____|____     
+"""
+
+    elif wrong_count == 8:
+        hangman = """
+        
+            _____
+            |/  
+            |   o
+            |  /|\ 
+            |   
+        ____|____     
+"""
+
+    elif wrong_count == 9:
+        hangman = """
+        
+            _____
+            |/  
+            |   o
+            |  /|\ 
+            |  / 
+        ____|____     
+"""
+
+    elif wrong_count == 10:
+        hangman = """
+        
+            _____
+            |/  
+            |   o
+            |  /|\ 
+            |  / \ 
+        ____|____     
+"""
+
+    elif wrong_count == 11:
+        hangman = """
+        Game over!!!
+            _____
+            |/  |
+            |   o
+            |  /|\ 
+            |  / \ 
+        ____|____     
+"""
+    return hangman
+
+
+    
 
 
 
 def game_logic(game_blanks, game_word):
     #Game while loop
     #print gamestate
-    print("Enter a letter: ")
-    new_letter = input()
+    game_fail = False
+    wrong_count = 0
+    while game_fail == False:
+        print("Enter a letter: ")
+        new_letter = input()
 
-    for letter in game_word:
-        if letter == new_letter:
-            print("Correct!")
-            #update_blanks(new_letter, game_blanks, game_word)
-            #draw new board
-            #print new blanks
-        else:
-            print("Wrong answer!")
-            #update_board()
+        for letter in game_word:
+            if letter == new_letter:
+                print("Correct!")
+                #update_blanks(new_letter, game_blanks, game_word)
+                #draw new board
+                #print new blanks
+            else:
+                print("Wrong answer!")
+                wrong_count +=1
+                #update_board(wrong_count)
          
-game_title()
+""" game_title()
 #game_intro()
-new_word = get_word(3)
-print(new_word)
+new_word = get_word(2)
+print(new_word) """
+
+
+x = update_board(5)
+print(x)
